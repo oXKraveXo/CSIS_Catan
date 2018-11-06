1efe6d709577f2d3f2d6e70089a1d14b62cf405d
Tyler Perry <Tyler Perry@192.168.1.100>
2018-11-06 15:42:45 -0600

    # WARNING: head commit changed in the meantime
    Day 1 Add_ins

--- UserGUI.java
+++ UserGUI.java
@@ -8,100 +8,71 @@ public class UserGUI extends JFrame{
 	private static int HEIGHT = 550;
 	
 	// Class variables 
-	private int NumPlayers;
-	private String Condiction;
-	private String [] PlayersNames = new String[4];
-	private String [] PlayersColors = new String[4];
-	private int [] Order = new int[4];
-	private String Chites,LandRandom;
+	User user = getUser();
 	
-	// Class variables 
-	String name;
-	char color;
-	int orderNum;
-	int settlements;
-	int cities;
-	int roads;
+	// Class variables for use in GUI
+	String name = user.getName();
+	char color = user.getColor();
+	int orderNum = user.getOrderNum();
+	int settlements = user.getSettlements();
+	int cities = user.getCities();
+	int roads = user.getRoads();
 	//Cards [] usersCards; 
-	int condiction;
-	int [] resorces; 
-	
-	// 
-	
+	int condiction = user.getCondiction();
+	int [] resorces = user.getResorces(); 
 	
 	// GUI variables
-	private JTextField [] Players = new JTextField[4];
-	private JTextField [] Colors = new JTextField[4];
-	private JLabel [] PlayersLabel= new JLabel[4];
-	private JLabel [] ColorLabel= new JLabel[4];
 	private JTextArea errorArea;
-	private JLabel chitesLabel;
-	private JTextField chitesField;
-	private JLabel landLabel;
-	private JTextField landField;
+	private JLabel nameLabel;
+	private JLabel colorLabel;
+	private JLabel orderNumLabel;
+	private JLabel settlementsLabel;
+	private JLabel citiesLabel;
+	private JLabel roadsLabel;
 	private JLabel condictionLabel;
-	private JTextField condictionField;
+	private JLabel resorcesLabel;
 	private JButton build;
 	
 	public UserGUI(){// the constructor for the GUI
-		setTitle("Hello Welcome to Catan");
+		setTitle("Hello Welcome to User Interface");
 		setSize(WIDTH, HEIGHT);
 		
 		Container pane = getContentPane();
 		pane.setLayout(null); 
 		
 		//Instantiate objects of GUI components
-		for (int i = 0; i<4; i++ ) {
-			PlayersLabel [i]= new JLabel("Name of player " +i);
-			Players[i] = new JTextField();
-			ColorLabel[i] = new JLabel("Color of player " +i);
-			Colors[i] = new JTextField();
-		}
 		
 		errorArea = new JTextArea("Message If You Fuck Up"); 
-		chitesLabel = new JLabel("Do you want to randomize the chites");
-		chitesField = new JTextField();
-		landLabel = new JLabel("Do you want the land to be randomize");
-		landField = new JTextField();
-		condictionLabel = new JLabel("Win Condiction");
-		condictionField = new JTextField();
+		nameLabel = new JLabel("This is the name "+ name);
+		colorLabel = new JLabel("This is the this color "+ color);
+		orderNumLabel = new JLabel("This is the orderNum "+ orderNum);
+		settlementsLabel = new JLabel("This is the settlements "+ settlements);
+		citiesLabel = new JLabel("This is the cities "+ cities);
+		roadsLabel = new JLabel("This is the roads "+ roads);
+		condictionLabel = new JLabel ("This is the users condiction poiint "+ condiction);
+		resorcesLabel = new JLabel ("Wheet "+resorces[0]+ " Stone " +resorces[1]+ " Clay " +resorces[2] + " Sheep " + resorces[3] + " Wood " + resorces[4] );
 		build = new JButton("Build");
 
 		processButtonHandler processBHandler = new processButtonHandler();
 		build.addActionListener(processBHandler);
 		
 		// Method for location and size of objects
-		int counter = 1;
-		for (int i = 0; i<4; i++ ) {
-			int temp = counter * 20;
-			int width = temp + 200;
-			int width2 = temp + 300; 
-			
-			Players [i].setLocation(300,width);
-			Players [i].setSize(200,20);
-			PlayersLabel [i].setSize(200,20);
-			PlayersLabel [i].setLocation(40,width);
-			Colors [i].setSize(200,20);
-			Colors [i].setLocation(300,80+width);
-			ColorLabel [i].setSize(200,20);
-			ColorLabel [i].setLocation(40,80+width);
-			counter++;
-		}
-		
-		chitesLabel.setLocation(40,380);
-		chitesLabel.setSize(200,20);
-		landLabel.setLocation(40,400);
-		landLabel.setSize(200,20);
-		condictionLabel.setLocation(40,420);
-		condictionLabel.setSize(200,20);
-		
-		chitesField.setLocation(300,380);
-		chitesField.setSize(200,20);
-		landField.setLocation(300,400);
-		landField.setSize(200,20);
-		condictionField.setLocation(300,420);
-		condictionField.setSize(200,20);
-		
+		nameLabel.setLocation(40,200);
+		nameLabel.setSize(200,20);
+		colorLabel.setLocation(40,220);
+		colorLabel.setSize(200,20);
+		orderNumLabel.setLocation(40,240);
+		orderNumLabel.setSize(200,20);
+		settlementsLabel.setLocation(40,260);
+		settlementsLabel.setSize(200,20);
+		citiesLabel.setLocation(40,280);
+		citiesLabel.setSize(200,20);
+		roadsLabel.setLocation(40,300);
+		roadsLabel.setSize(200,20);
+		condictionLabel.setLocation(40,320);
+		condictionLabel.setSize(400,20);
+		resorcesLabel.setLocation(40,340);
+		resorcesLabel.setSize(400,20);
 		errorArea.setLocation(40,10);
 		errorArea.setSize(470,180);
 		build.setLocation(200,440);
@@ -109,19 +80,15 @@ public class UserGUI extends JFrame{
 		
 		
 		// Adding objects to container
-		for (int i = 0; i<4; i++) {
-			pane.add(Players[i]);
-			pane.add(PlayersLabel[i]);
-			pane.add(Colors[i]);
-			pane.add(ColorLabel[i]);
-		}
 		pane.add(errorArea);
-		pane.add(chitesLabel);
-		pane.add(chitesField);
-		pane.add(landLabel);
-		pane.add(landField);
+		pane.add(nameLabel);
+		pane.add(colorLabel);
+		pane.add(orderNumLabel);
+		pane.add(settlementsLabel);
+		pane.add(citiesLabel);
+		pane.add(roadsLabel);
 		pane.add(condictionLabel);
-		pane.add(condictionField);
+		pane.add(resorcesLabel);
 		pane.add(build);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -132,19 +99,24 @@ public class UserGUI extends JFrame{
 	private class processButtonHandler implements ActionListener{//class to handle the Button
 		@Override
 		public void actionPerformed(ActionEvent arg0) {// the method of the button controls
-			Chites = chitesField.getText();
-			LandRandom = landField.getText();
-			Condiction = condictionField.getText();
-			
-			for (int i = 0; i < 4; i++) {
-			PlayersNames [i]= Players[i].getText(); 
-			PlayersColors [i] = Colors[i].getText();
-			}
-			for (int i = 0; i<4; i++) {
-				System.out.println("Test Name " + PlayersNames [i] + " Test Color " + PlayersColors [i] + " Test Order " + i );
-			}
-			System.out.println("Chites " + Chites + " Land " + LandRandom + " Condiction " + Condiction);
-				//errorArea.setText("This is the error area \n" +"You have to many mines in your Game \n"+ "Name : "+name + "\nBoxes :" +box + "\nMines :"+mine);
+			System.out.println("This is a test");
 		}
 		}
+	public User getUser() {
+		String name = "TestName";
+		char color = 'B';
+		int orderNum = 1;
+		int settlements = 5;
+		int cities = 4;
+		int roads = 15;
+		Cards [] usersCards = new Cards [20];
+		for (int i = 0; i< 10; i++)
+			usersCards[i] = new Cards("temp", 5);
+		int condiction = 10;
+		int [] resorces = new int [5]; 
+		for (int i = 0; i<resorces.length; i++)
+			resorces[i] = i;
+		User object = new User(name, color, orderNum, settlements, cities, roads, usersCards, condiction, resorces);
+		return object;
+	}
     }
